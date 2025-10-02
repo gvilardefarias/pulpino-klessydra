8fb73f8e3aa0c76aaeec7a7f635d0e1147f6ecf1
Gustavo Vilar de Farias <gustavo.vilarde@polito.it>
2025-10-02 11:12:25 +0200

    Fixing jasper script

--- jasper/safeFaults/jasperRun_final_DSP.tcl
+++ jasper/safeFaults/jasperRun_final_DSP.tcl
@@ -1,7 +1,7 @@
 source jasper_aux_functions.tcl
 
 # Set name of TOP Instance of the design to be considered by Jasper
-set topInstance "ADDER"
+set topInstance "DSP"
 
 # set resetSignal "wb_rst_i"
 
@@ -28,22 +28,65 @@ elaborate
 # Setting Initialization settings
 #########################
 
-# clock -infer
-clock -none
+clock -infer
+#clock -none
+
+set LIST_OF_CELLSDEFINE [get_design_info -list celldefine]
+set LIST_OF_UDPS        [get_design_info -list prim_mod]
+set LIST_OF_CELLS       {}
+
+foreach CD $LIST_OF_CELLSDEFINE {
+    set FOUND 0
+
+    foreach UDP $LIST_OF_UDPS {
+	if {$CD eq $UDP} {
+	    set FOUND 1
+	    break
+	}
+    }
+
+    if {$FOUND == 0} {
+	lappend LIST_OF_CELLS $CD
+    }
+}
+
+### Collect instances of the cells and their ports
+set LIST_OF_PORTS {}
+
+foreach CELL $LIST_OF_CELLS {
+
+    set ALL_CELL_INST [get_design_info -module $CELL -list instance -silent]
+    
+    foreach CELL_INST $ALL_CELL_INST {
+	### Skip instances related to bad_machine
+	if { [string first "$topInstance\_bad_machine" $CELL_INST] >= 0 } {
+	    continue
+	}
+
+	set ALL_CELL_PINS [get_design_info -instance $CELL_INST -list input output -silent]
+
+     	foreach CELL_PIN $ALL_CELL_PINS {
+     	    lappend LIST_OF_PORTS [concat $CELL_INST.$CELL_PIN]
+     	}
+    }
+}
+
+### instrument faults SA0/SA1
+check_fsv -fault -add $LIST_OF_PORTS -type sa0+sa1
 
 
 
 #reset $resetSignal 
 #reset [reset -analyze -synchronous -list signal -silent]
 
-reset -none
+#reset -none
 
 #sanity_check -analyze all
 
 #########################
 # Specify custom fault target list (all flops with SEU fault, all signals with other faults)
 #########################
-check_fsv -fault -add [get_design_info -list signal] -type SA0+SA1 
+#check_fsv -fault -add [get_design_info -list signal] -type SA0+SA1 
 #check_fsv -fault -add [get_design_info -list flop]   -type SEU -time_window 0:$
 #check_fsv -fault -add [get_design_info -list signal] -type SET -time_window 0:$ -set_hold_time 500ns
 #check_fsv -fault -remove [check_fsv -fault -list -node {.+_failure} -regexp ]
@@ -52,7 +95,12 @@ check_fsv -fault -add [get_design_info -list signal] -type SA0+SA1
 # Specify the custom strobe list using the DESIGN_TOP env variable
 #########################
 
-check_fsv -strobe -add {dsp_except_data dsp_taken_branch dsp_except_condition busy_dsp state_DSP dsp_we_word dsp_sc_read_addr dsp_to_sc dsp_sc_data_write_wire dsp_sc_write_addr dsp_sci_we dsp_sci_req} -functional
+#check_fsv -strobe -add {dsp_except_data dsp_taken_branch dsp_except_condition busy_dsp state_DSP dsp_we_word dsp_sc_read_addr dsp_to_sc dsp_sc_data_write_wire dsp_sc_write_addr dsp_sci_we dsp_sci_req} -functional
+#########################
+# Specify the custom strobe list as all outputs from topInstance variable
+#########################
+check_fsv -strobe -add [get_design_info -instance $topInstance -list output  -include_hier_path -silent] -functional
+
 
 set strobe_list [check_fsv -strobe -get node [check_fsv -strobe -list]]
 
